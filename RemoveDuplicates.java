8e3d52d52e1d3cd700db898582a6c0327ac86101
veeramanikandan2020 <64081412+veeramanikandan2020@users.noreply.github.com>
2023-05-15 12:58:44 +0530

    RemoveDuplicates.java

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -1,36 +1,24 @@
 package collections.program;
 
+import java.util.Iterator;
 import java.util.LinkedHashSet;
+
 import java.util.Set;
 
 public class RemoveDuplicates {
 
 	/*
-	 * Declare a String as "PayPal India"
-	 * 
-	 * Convert it into a character array
-	 * 
-	 * Declare a Set as charSet for Character
-	 * 
-	 * Declare a Set as dupCharSet for duplicate Character
-	 * 
-	 * Iterate character array and add it into charSet
-	 * 
-	 * if the character is already in the charSet then, add it to the dupCharSet
-	 * 
-	 * Check the dupCharSet elements and remove those in the charSet
-	 * 
-	 * Iterate using charSet
-	 * 
-	 * Check the iterator variable isn't equals to an empty space
-	 * 
-	 * print it
-	 * 
+	 * Declare a String as "PayPal India" Convert it into a character array Declare
+	 * a Set as charSet for Character Declare a Set as dupCharSet for duplicate
+	 * Character Iterate character array and add it into charSet if the character is
+	 * already in the charSet then, add it to the dupCharSet Check the dupCharSet
+	 * elements and remove those in the charSet Iterate using charSet Check the
+	 * iterator variable isn't equals to an empty space print it
 	 */
 
 	public static void main(String[] args) {
 
-		String str = "PayPal";
+		String str = "Pay Pal";
 
 		char[] ch = str.toCharArray();
 
@@ -42,17 +30,24 @@ public class RemoveDuplicates {
 
 			boolean add = charSet.add(ch[i]);
 
-			if (add) {
-				charSet.add(ch[i]);
-			} else {
+			if (!add) {
 				dupCharSet.add(ch[i]);
 			}
-
 		}
 
 		charSet.removeAll(dupCharSet);
 
-		System.out.println(charSet);
+		Iterator<Character> iterVar = charSet.iterator();
+
+		while (iterVar.hasNext()) {
+			char chVar = iterVar.next();
+			if((chVar != ' '))
+			{
+				System.out.println(chVar);
+			}
+			
+		}
+
 	}
 
 }
