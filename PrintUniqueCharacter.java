c085150fd16baf198d7ec1d4978dd244d5b2675c
veeramanikandan2020 <64081412+veeramanikandan2020@users.noreply.github.com>
2023-05-15 13:06:27 +0530

    PrintUniqueCharacter.java

--- PrintUniqueCharacter.java
+++ PrintUniqueCharacter.java
@@ -4,45 +4,54 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class PrintUniqueCharacter {
-
 	/*
-	 * Problem
-	 * 
-	 * a) Take your name as input b) Print all unique characters only (any order)
-	 * 
-	 * Input: babu Output: a u
-	 * 
+	 * Problem a) Take your name as input b) Print all unique characters only (any
+	 * order) Input: babu Output: a u
 	 */
-
 	/*
-	 * Psuedocode
-	 * 
-	 * a) Convert String to Character array b) Create a new Set -> HashSet c) Add
-	 * each character to the Set and if it is already there, remove it d) Finally,
-	 * print the set
-	 * 
+	 * Psuedocode a) Convert String to Character array b) Create a new Set ->
+	 * HashSet c) Add each character to the Set and if it is already there, remove
+	 * it d) Finally, print the set
 	 */
 
 	public static void main(String[] args) {
 
 		// String name = "babu";
-		String name = "veeramanikanda"; //when 3 character repeats it is not removed
 
-		char[] ch = name.toCharArray();
+		/*
+		 * String name = "veeramanikandan";
+		 * 
+		 * char[] ch = name.toCharArray();
+		 * 
+		 * Set<Character> setChar = new HashSet<Character>();
+		 * 
+		 * 
+		 * 
+		 * for (int i = 0; i < ch.length; i++) {
+		 * 
+		 * boolean add = setChar.add(ch[i]);
+		 * 
+		 * if (add) { setChar.add(ch[i]); } else { setChar.remove(ch[i]); } } //when n
+		 * character repeats 3 times it is not removed System.out.println(setChar);
+		 */
 
-		Set<Character> setChar = new HashSet<Character>();
+		String s = "veeramanikandan";
 
-		for (int i = 0; i < ch.length; i++) {
+		char[] charArray = s.toCharArray();
 
-			boolean add = setChar.add(ch[i]);
+		Set<Character> letters = new HashSet<Character>();
+		Set<Character> dupletters = new HashSet<Character>();
 
-			if (add) {
-				setChar.add(ch[i]);
-			} else {
-				setChar.remove(ch[i]);
+		// print duplicate
+		for (int i = 0; i < charArray.length; i++) {
+			boolean add = letters.add(charArray[i]);
+			if (!add) {
+				dupletters.add(charArray[i]);
 			}
 		}
-		System.out.println(setChar);
+		//System.out.println(dupletters);
+		letters.removeAll(dupletters);
+		System.out.println(letters);
 
 	}
 
